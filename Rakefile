0fb97b1287c42b806724eca17e82766cbfb680d6
Dan Croak <dcroak@thoughtbot.com>
2010-01-11 23:35:39 -0500

    gemifying

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'jeweler'
+
+Jeweler::Tasks.new do |gem|
+  gem.name        = "sinatra-effigy"
+  gem.summary     = "An Effigy extenstion for Sinatra."
+  gem.description = "HTML in .html files. Ruby in .rb files."
+  gem.email       = "dcroak@thoughtbot.com"
+  gem.homepage    = "http://github.com/dancroak/sinatra-effigy"
+  gem.authors     = ["Dan Croak"]
+
+  gem.add_dependency("sinatra")
+end
+
+Jeweler::GemcutterTasks.new
--- lib/sinatra/effigy.rb
+++ lib/sinatra/effigy.rb
@@ -3,13 +3,15 @@ require 'effigy'
 
 module Sinatra
   module EffigyHelper
-    module Helpers
-      def effigy(name)
-        template   = File.read(File.join(options.root, "templates", "#{name}.html"))
-        camel_name = "#{name}_view".gsub(' ', '_').gsub(/(?:^|_)(.)/) { $1.upcase }
-        view       = Object.const_get(camel_name).new
-        view.render(template)
-      end
+    if File.directory?('views')
+      Dir['views/*'].each {|view| require view }
+    end
+
+    def effigy(name)
+      template   = File.read(File.join(options.root, "templates", "#{name}.html"))
+      camel_name = "#{name}_view".gsub(' ', '_').gsub(/(?:^|_)(.)/) { $1.upcase }
+      view       = Object.const_get(camel_name).new
+      view.render(template)
     end
   end
 
