b99d3016623b948588b62e03fe8da4a3087c57d8
Danny Tatom <dannytatom@gmail.com>
2010-03-08 18:17:42 -0800

    You can now set a templates directory with

--- lib/sinatra/effigy.rb
+++ lib/sinatra/effigy.rb
@@ -2,19 +2,27 @@ require 'sinatra/base'
 require 'effigy'
 
 module Sinatra
-  module EffigyHelper
-    Dir['views/*'].each {|view| require view }
+  module Effigy
+    module Helpers
+      Dir['views/*'].each { |view| require view }
 
-    def effigy(name, *locals)
-      camel_name = "#{name}_view".
-                      gsub(' ', '_').
-                      gsub(/\/(.)/)  { "::#{$1.upcase}" }.
-                      gsub(/(?:^|_)(.)/) { $1.upcase }
-      view     = Object.const_get(camel_name).new(*locals)
-      template = File.read("templates/#{name}.html")
-      view.render(template)
+      def effigy(name, *locals)
+        camel_name = "#{name}_view".
+                        gsub(' ', '_').
+                        gsub(/\/(.)/)  { "::#{$1.upcase}" }.
+                        gsub(/(?:^|_)(.)/) { $1.upcase }
+        view      = Object.const_get(camel_name).new(*locals)
+        template  = File.read("#{self.options.templates}/#{name}.html")
+        view.render(template)
+      end
+    end
+
+    def self.registered(app)
+      app.helpers Effigy::Helpers
+
+      app.set :templates, 'templates'
     end
   end
 
-  helpers EffigyHelper
+  register Effigy
 end
