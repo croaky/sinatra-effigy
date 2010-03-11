13e0f631b96f0f3a683e79c360adedac14f98564
Dan Lawless <lawless@dan-lawlesss-macbook.local>
2010-03-11 13:56:32 -0500

    support for views override

--- lib/sinatra/effigy.rb
+++ lib/sinatra/effigy.rb
@@ -4,25 +4,24 @@ require 'effigy'
 module Sinatra
   module Effigy
     module Helpers
-      def initialize
-        Dir["#{self.options.views}/*"].each { |view| require view }
-      end
-
       def effigy(name, *locals)
         camel_name = "#{name}_view".
                         gsub(' ', '_').
                         gsub(/\/(.)/)  { "::#{$1.upcase}" }.
                         gsub(/(?:^|_)(.)/) { $1.upcase }
         view      = Object.const_get(camel_name).new(*locals)
-        template  = File.read("#{self.options.templates}/#{name}.html")
+        template  = File.read("#{options.templates}/#{name}.html")
         view.render(template)
       end
     end
 
     def self.registered(app)
       app.helpers Effigy::Helpers
-      
+
       app.set :templates, 'templates'
+      app.set :views,     'views'
+
+      Dir["#{app.views}/*"].each { |view| require view }
     end
   end
 
